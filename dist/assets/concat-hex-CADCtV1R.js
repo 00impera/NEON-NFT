aa6b633162e8b12981ca82c4f51353f536a62b0d
00impera <nelutz2you@gmail.com>
2026-04-28 02:51:59 +0200

    Fix vite config - bundle thirdweb

---
+++ dist/assets/concat-hex-CADCtV1R.js
@@ -0,0 +1,1 @@
+import{q as t}from"./index-D1FZVMsm.js";function n(o){return t(...o)}export{n as concatHex};
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: './',
-  resolve: {
-    alias: {
-      'thirdweb/react': path.resolve('./node_modules/thirdweb/dist/esm/react'),
-      'thirdweb': path.resolve('./node_modules/thirdweb/dist/esm'),
-    }
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom'],
-    exclude: ['thirdweb']
-  },
   build: {
     rollupOptions: {
-      external: ['thirdweb', 'thirdweb/react'],
+      output: {
+        manualChunks: undefined
+      }
     }
   }
 })
